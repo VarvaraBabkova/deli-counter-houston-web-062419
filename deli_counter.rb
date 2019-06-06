5f184b248363fc8b7f5b3f0412e680176243fa22
Varvara Babkova <varvara.munday@gmail.com>
2019-06-06 23:04:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,7 @@ def line (l)
   if l.size == 0
     puts "The line is currently empty."
   end
-  
+  str = "The line is currently: "
+  n = 0
+  l.each {|name| str += (n.to_s + ". " + name.to_s)}
 end
